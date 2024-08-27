dced7bd4a7a5587f34165a39b2dd48b9a098278d
Marcos Monteiro <marcos.moliveira@outlook.com>
2024-08-27 12:06:21 -0300

    finalizando componete

--- src/components/MenuCard.tsx
+++ src/components/MenuCard.tsx
@@ -1,16 +1,19 @@
 import { TouchableOpacity, TouchableOpacityProps } from "react-native";
-import { HStack, Image, VStack, Text } from "native-base";
+import { HStack, Image, VStack, Text, Center } from "native-base";
 
-import { menuDTO } from "../dtos/menuDTO";
-
+import CaloriesIco from '../assets/fire_115156.svg';
+export type menuDTO = {
+    id: string
+    nome: string
+    caloria: number
+    valor: number
+    image: string | null
+}
 type props = TouchableOpacityProps & {
     data: menuDTO
 }
-
-import menuDefault from '../assets/menuDefault.png'
-
+import menuDefault from '../assets/menuDefault.png';
 export function MenuCard({ data, ...rest }: props) {
-
     return (
         <TouchableOpacity
             // onPress={()}
@@ -20,10 +23,12 @@ export function MenuCard({ data, ...rest }: props) {
             <HStack ml={10} mt={2} w='75%' borderRadius={10} alignItems="center">
                 <VStack ml={2} flex={1}>
                     <Text fontSize="20px" fontWeight={"bold"} mt={2}>{data.nome}</Text>
-                    <Text fontSize="15px" mt={2}>{data.caloria} Calorias</Text>
-                    <Text fontSize="15px" mt={2}>R$ {data.valor}</Text>
+                    <HStack alignItems={"center"}>
+                        <CaloriesIco width={28} height={28}/>
+                        <Text fontSize="15px" mt={2}>{data.caloria} Calorias</Text>
+                    </HStack>
+                    <Text fontSize="15px" color={"yellow.600"} mt={2}>R$ {data.valor}</Text>
                 </VStack>
-
                 <Image
                     source={data.image ? {uri: data.image} : menuDefault}
                     alt={data.nome}
